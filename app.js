bcb56a5f047fa2f801131c547b5f67d63035443b
Jack Agtual <jaagtual@gmail.com>
2022-10-17 20:48:49 -0700

    Adding function to add book to library

--- app.js
+++ app.js
@@ -11,3 +11,24 @@ class Book {
 
 const addBookToLibrary = (...arguments) => library.push(new Book(...arguments));
 
+const renderLibrary = () => {
+    const libDiv = document.querySelector('.books');
+
+    library.map(bookObj => {
+        const book = document.createElement('div');
+        book.classList.add('book');
+
+        book.innerHTML = `
+            <h3 class="title">${bookObj.title}</h3>
+            <p>${bookObj.author}</p>
+            `;
+
+        libDiv.appendChild(book);
+    });
+
+}
+
+addBookToLibrary('title1', 'author1', 3, false);
+addBookToLibrary('title2', 'author2', 4, true);
+
+renderLibrary();
